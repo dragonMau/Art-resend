e82b52b8e58b6b162246d8432e937310738319e8
dragonMau <73312635+dragonMau@users.noreply.github.com>
2022-10-20 22:32:16 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+urls = [
+"https://discord.com/api/webhooks/hookA",
+"https://discord.com/api/webhooks/hookB"
+]
+data = {
+    "username" : "mine discord username",
+    "avatar_url" : "https://cdn.discordapp.com/avatars/mine avatar",
+    "id" : mine id (int)
+}
+bank = bank channel id (int)
+token = "token of bank manager bot"
